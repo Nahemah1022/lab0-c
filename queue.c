aa498f4215d369eb5010fe32569b5520f5be3c78
Nahemah1022 <a122774007@gmail.com>
2021-03-06 21:56:51 +0800

    Implement q_sort by non-recursive quick sort

--- queue.c
+++ queue.c
@@ -177,6 +177,49 @@ void q_reverse(queue_t *q)
  */
 void q_sort(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if (!q || !q->head || !q->head->next) {
+        return;
+    }
+
+    char *pivot;
+    int i = 0;
+    list_ele_t *beg[q->size], *end[q->size];
+    list_ele_t *l, *r, *pr;
+    beg[0] = q->head;
+    end[0] = q->tail;
+    while (i >= 0) {
+        l = beg[i];
+        r = end[i];
+        if (l && r && l != r) {
+            pivot = l->value;
+            while (l != r) {
+                pr = r = l;
+                while (pr != end[i]) {  // find the last one smaller than pivot
+                    pr = pr->next;
+                    if (strcasecmp(pr->value, pivot) < 0)
+                        r = pr;
+                }
+
+                if (r != l) {  // found
+                    l->value = r->value;
+                    l = l->next;
+                }
+
+                while (strcasecmp(l->value, pivot) <= 0 &&
+                       l != r) {  // find the first one bigger than pivot
+                    l = l->next;
+                }
+
+                if (l != r) {
+                    r->value = l->value;
+                }
+            }
+            l->value = pivot;
+            beg[i + 1] = l->next;
+            end[i + 1] = end[i];
+            end[i++] = l;
+        } else {
+            i--;
+        }
+    }
 }
